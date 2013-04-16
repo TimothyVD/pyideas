8bfc4c11a21815c769de5b0283cb55a912f25dc8
stijnvanhoey <stvhoey.vanhoey@ugent.be>
2013-04-16 16:17:24 +0200

    optimalitey design criteria added

--- optimalexperimentaldesign.py
+++ optimalexperimentaldesign.py
@@ -38,6 +38,9 @@ class OED(odegenerator):
     '''
     def __init__(self,System, Parameters, Modelname = 'MyModel'):
         odegenerator.__init__(self, System, Parameters, Modelname = Modelname)
+        
+        self.criteria_optimality_info = {'A':'min', 'modA': 'max', 'D': 'max', 
+                                         'E':'max', 'modE':'min'}
     
     
     def set_measured_times(self, meas_time):
@@ -58,7 +61,7 @@ class OED(odegenerator):
             print 'not yet implemented'
     
     def set_measured_errors(self, meas_error_dict, method = 'relative'):
-        '''CURRENTLY UNITY
+        '''CURRENTLY UNITY MATRIX!!!!
         Measurement errors on the measured data; options are relative and
         absolute.
 
@@ -147,12 +150,101 @@ class OED(odegenerator):
             #calculate matrices
             FIMt = np.matrix(sensmatrix).transpose() * np.matrix(self.Qerr) * np.matrix(sensmatrix)
             self.FIM = self.FIM + FIMt
-            
+        return self.FIM
+
+    def _check_for_FIM(self):
+        ''' help function for FIM testing
+        If FIM exists, ok, otherwise start FIM calculation
+        '''  
+        try:
+            self.FIM
+        except:
+            print 'FIM matrix is calculated...'
+            self.get_FIM()
+            print '... done!'
+
+    def A_criterium(self):
+        '''OED design A criterium
+        With this criterion, the trace of the inverse of the FIM is minimized, 
+        which is equivalent to minimizing the sum of the variances of the 
+        parameter estimates. In other words, this criterion minimizes the 
+        arithmetic average of the variances of the parameter estimate. 
+        Because this criterion is based on an inversion of the FIM, 
+        numerical problems will arise when the FIM is close to singular.        
+        '''
+        self._check_for_FIM()
+        print 'MINIMIZE A criterium for OED'
+        return self.FIM.I.trace()
+        
+    def modA_criterium(self):
+        '''OED design modified A criterium
+        With this criterion, the trace of the inverse of the FIM is minimized, 
+        which is equivalent to minimizing the sum of the variances of the 
+        parameter estimates. In other words, this criterion minimizes the 
+        arithmetic average of the variances of the parameter estimate. 
+        Because this criterion is based on an inversion of the FIM, 
+        numerical problems will arise when the FIM is close to singular.        
+        '''
+        self._check_for_FIM()
+        print 'MAXIMIZE modA criterium for OED'
+        return self.FIM.trace()                   
+        
     def D_criterium(self):
-        pass
-    
+        '''OED design D criterium
+        Here, the idea is to maximize the determinant of the FIM 
+        (Box and Lucas, 1959). The latter is inversely proportional to the 
+        volume of the confidence region of the parameter es- timates, and this 
+        volume is thus minimized when maximizing det (FIM). In other words, 
+        one minimizes the geometric average of the variances of the parameter 
+        estimates. More- over, D-optimal experiments possess the property of 
+        being invariant with respect to any rescaling of the parameters 
+        (Petersen, 2000; Seber and Wild, 1989). According to Walter and 
+        Pronzato (1997), the D-optimal design criterion is the most used 
+        criterion. However, several authors have pointed out that this 
+        criterion tends to give excessive importance to the parameter which 
+        is most influential.
+        '''
+        self._check_for_FIM()
+        print 'MAXIMIZE D criterium for OED'
+        return np.linalg.det(self.FIM)          
+
+    def E_criterium(self):
+        '''OED design E criterium
+        The E-optimal design criterion maximizes the smallest eigenvalue of 
+        the FIM and thereby minimizes the length of the largest axis of the 
+        confidence ellipsoid. Thus, these designs aim at minimizing the 
+        largest parameter estimation variance and thereby at maximizing the 
+        distance from the singular, unidentifiable case.
+        '''
+        self._check_for_FIM()
+        print 'MAXIMIZE E criterium for OED'
+        w, v = np.linalg.eig(self.FIM)
+        return min(w)
+
+    def modE_criterium(self):
+        '''OED design modE criterium
+        With this criterion, the focus is on the minimization of the condition 
+        number, which is the ratio between the largest and the smallest 
+        eigenvalue, or, in other words, the ratio of the shortest and the 
+        longest ellipsoid axes. The minimum of this ratio is one, which 
+        corresponds to the case where the shape of the confidence ellipsoid 
+        is a (hyper)sphere.
+        '''
+        self._check_for_FIM()
+        print 'MINIMIZE modE criterium for OED'
+        w, v = np.linalg.eig(self.FIM)
+        return max(w)/min(w)
 
 
+    def get_all_optimality_design_criteria(self):
+        '''Return all optimality criteria
+        
+        '''
+        self._check_for_FIM()
+        self._all_crit = {'A':self.A_criterium(), 'modA': self.modA_criterium(), 
+                          'D': self.D_criterium(), 'E':self.E_criterium(), 'modE':self.modE_criterium()}
+        return self._all_crit
+              
     def set_measurement_data(self):
         '''
         Give measurement data to calculate objective functions and perform
