a3f763bbe70d7524890a3e6c4c02c627017b10a2
stijnvanhoey <stvhoey.vanhoey@ugent.be>
2013-06-19 13:55:58 +0200

    package load style for python pakages by __init__ file

---
+++ __init__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+@author: Timothy Van Daele & Stijn Van Hoey
+
+pySTAN: python STructure Analyst
+E-mail: stvhoey.vanhoey@ugent.be
+"""
+
+from ode_generator import odegenerator
+from optimalexperimentaldesign import *
+
+from plotfunctions import *
+from measurements import *
+
+__version__ = "0.01"
+
+
+if __name__ == '__main__':
+    print 'Bio-Intense ODE/OED package: python package for model development with Ordinary Differential Equations and Optimal Experimental Design'
