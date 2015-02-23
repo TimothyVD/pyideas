22c2e316f392bb0dfc3c0bb30fbd08502ddac50e
Timothy Van Daele <timothy.vandaele@ugent.be>
2015-02-23 17:47:15 +0100

    Add example how to use confidence class

---
+++ examples/modsim_confidence.py
@@ -0,0 +1,88 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb 17 19:20:53 2015
+
+@author: timothy
+"""
+import numpy as np
+import pandas as pd
+import os
+
+import biointense
+from biointense.model import AlgebraicModel
+from biointense.sensitivity import NumericalLocalSensitivity
+from biointense.confidence import BaseConfidence
+
+from pandas.util.testing import assert_frame_equal, assert_almost_equal
+
+
+def run_modsim_models_old():
+
+    # Data
+    file_path = os.path.join(biointense.BASE_DIR, '..', 'examples', 'data',
+                             'grasdata.csv')
+    data = pd.read_csv(file_path, header=0, names=['time', 'W'])
+    measurements = biointense.ode_measurements(data)
+
+    # Logistic
+
+    Parameters = {'W0': 2.0805,
+                  'Wf': 9.7523,
+                  'mu': 0.0659}
+
+    Alg = {'W': 'W0*Wf/(W0+(Wf-W0)*exp(-mu*t))'}
+
+    M1 = biointense.DAErunner(Parameters=Parameters, Algebraic=Alg,
+                              Modelname='Modsim1', print_on=False)
+
+    M1.set_xdata({'start': 0, 'end': 72, 'nsteps': 1000})
+    M1.set_measured_states(['W'])
+
+    optim1 = biointense.ode_optimizer(M1, measurements, print_on=False)
+    optim1.local_parameter_optimize(add_plot=False)
+
+    FIM_stuff1 = biointense.ode_FIM(optim1, print_on=False)
+    FIM_stuff1.get_newFIM()
+
+    return FIM_stuff1
+
+
+def run_modsim_models_new():
+
+    parameters = {'W0': 2.0805,
+                  'Wf': 9.7523,
+                  'mu': 0.0659}
+
+    system = {'W': 'W0*Wf/(W0+(Wf-W0)*exp(-mu*t))'}
+
+    M1 = AlgebraicModel('Modsim1', system, parameters)
+
+    M1.set_independent({'t': np.array([0., 20., 29., 41., 50., 65., 72.])})
+
+    M1.set_variables_of_interest(['W'])
+
+    M1.initialize_model()
+
+    M1.run()
+
+    M1sens = NumericalLocalSensitivity(M1, parameters.keys(), perturbation=1e-6)
+    sens_PD = M1sens.get_sensitivity(method='CAS')
+
+    M1conf = BaseConfidence(sens_PD, parameters)
+
+    error = np.zeros([1, len(M1._independent_values['t'])]) + 1
+    M1conf.uncertainty_PD = pd.DataFrame(np.concatenate(
+        [np.atleast_2d(M1._independent_values['t']),
+         error], axis=0).T, columns=['t', 'W']).set_index('t')
+    M1conf.FIM
+
+    return M1conf
+
+if __name__ == "__main__":
+    FIM_old = run_modsim_models_old()
+    FIM_new = run_modsim_models_new()
+    # np.testing.assert_almost_equal(FIM_old.FIM, FIM_new.FIM, decimal=7)
+    # assert_frame_equal(FIM_old.get_parameter_confidence(),
+    #                    FIM_new.get_parameter_confidence())
+    # assert_frame_equal(FIM_old.get_parameter_correlation(),
+    #                   FIM_new.get_parameter_correlation())
